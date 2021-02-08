e97ab11842e85da00d600fbe898ccb21dce64f93
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-02-08 21:29:38 +0700

    FinalComment

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ print()
 sum = 0
 kol = 0
 N = settings['N']  # кол-во итераций
-a = settings['a']  # начало интервала
-b = settings['b']  # конец интервала
+a = settings['a']  # начало интервала(всегда 0)
+b = settings['b']  # конец интервала()
 
 
 def write_in_file():
@@ -42,20 +42,20 @@ def disper():
     result = 1 / (N - 1) * result
     return result
 
-
+#a+(b-a)*R
 count_segemnts = math.ceil(1 + 3.22 * math.log(N))  # кол-во отрезков
 list_elements = list()
 for i in range(N):
-    num1 = random.randint(a, b)  # Случайное число от a До b
+    num1 = random.random() * b
     list_elements.append(num1)
     sum += num1
 
-local_segment = count_segemnts
+
 
 numbers1 = []
 for i in range(count_segemnts):
     numbers1.append(0)
-
+local_segment = (b-a)/count_segemnts
 i = 0
 temp_sorted_list_element = sorted(list_elements)
 for item in temp_sorted_list_element:
@@ -68,8 +68,12 @@ variable = count_segemnts / N
 ideal = N / count_segemnts
 
 result = 0
+j = 1
 for item in range(len(numbers1) - 1):
+    print('кол-во попаданий в ' + str(j) + ' интервале')
+    print(numbers1[item])
     result += numbers1[item] - ideal
+    j += 1
 
 print("Кол-во интервалов:")
 print(count_segemnts)
